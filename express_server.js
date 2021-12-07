cba480a9ada46e7c6225f9ead328601bb6d740d7
Bita <bita.janzadeh@hotmail.com>
2021-12-06 22:27:39 -0500

    Redirect Short URLs to original long URL

--- express_server.js
+++ express_server.js
@@ -54,7 +54,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
