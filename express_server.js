feacc7a266961ce2f7355c241a92fd92cbaaac3e
Bita <bita.janzadeh@hotmail.com>
2021-12-07 23:05:15 -0500

    Display the Username in header

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
